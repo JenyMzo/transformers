6ef3bd48f3b1c9bda48c6e45abe383b0438971ec
Ogun Isik <ogunisik@gmail.com>
2019-11-05 14:02:15 +0100

    add ogun as a new team member

--- assets/data/teamMembers.js
+++ assets/data/teamMembers.js
@@ -1,11 +1,24 @@
 module.exports = {
   jenymazo: {
-    name: 'Jeny Mazo',
-    rol: 'Fullstack Developer',
-    bio: 'Javascript Developer in love with NodeJS, passionate about new technologies and challenges',
-    twitter: 'JenyMzo',
-    github: 'JenyMzo',
-    avatar: 'https://avatars3.githubusercontent.com/u/2567952?v=3&u=93af511467354af56abba97d969cccca6fdffbef&s=250',
-    website: 'https://twitter.com/JenyMzo'
+    name: "Jeny Mazo",
+    rol: "Fullstack Developer",
+    bio:
+      "Javascript Developer in love with NodeJS, passionate about new technologies and challenges",
+    twitter: "JenyMzo",
+    github: "JenyMzo",
+    avatar:
+      "https://avatars3.githubusercontent.com/u/2567952?v=3&u=93af511467354af56abba97d969cccca6fdffbef&s=250",
+    website: "https://twitter.com/JenyMzo"
+  },
+  ogun: {
+    name: "Ogün Isik",
+    rol: "Backend Developer",
+    bio:
+      "Former C# engineer turned DevOps engineer who is trying to understand Node.js and Vue.js and a lot of other things.",
+    twitter: "ogunisik",
+    github: "ogun",
+    avatar:
+      "https://pbs.twimg.com/profile_images/378800000185077282/bc6e5c3817233d28405f32ed01125e48_400x400.png",
+    website: "https://github.com/ogunisik"
   }
-}
+};
